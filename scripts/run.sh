faa41b516d14f5a5f85e636f52e897032ba97d1a
gesa <gesanei87@gmail.com>
2025-10-07 09:50:32 +0000

    switch to gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,5 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+# app/wsgin.py
+gunicorn app.wsgi:application --bind :8000 --workers 3
